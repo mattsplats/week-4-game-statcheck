d9e48059b3e397f464200d77e50b0b4369a8fc39
Matt Davis <matt_davis@me.com>
2016-08-19 17:25:11 -0500

    Code cleanup with comments!

--- check.js
+++ check.js
@@ -4,23 +4,25 @@ $("button").on('click', function() {
 });
 
 function checkStats() {
-	var charArray = [{}, {}, {}, {}];
+	var charArray = [{}, {}, {}, {}];  // Holds all our character objects
 
+	// Pulls all character data from page input fields, stores in charArray
 	for (var i = 0; i < 4; i++) {
 		charArray[i].index = i;
 		charArray[i].name = $("#name_" + i).val();
 		charArray[i].hp = parseInt($("#hp_" + i).val());
 		charArray[i].baseAttack = charArray[i].currentAttack = parseInt($("#attack_" + i).val());
 		charArray[i].counter = parseInt($("#counter_" + i).val());
-		charArray[i].won = false;
+		charArray[i].won = false;  // Boolean return for doBattle()
 	}
 
+	// For every character
 	for (var i = 0; i < 4; i++) {
-		var resultArr = [];
+		var resultArr = [];  // Stores order of three successful fights
 		
-		checkAllEnemies([], charArray[i]);
+		checkAllEnemies([], charArray[i]);  // Check all fight order possibilities, stores in resultArr
 
-		// debugger;
+		// Prints all orderings (or none found)
 		if (resultArr.length > 0) {
 			for (var j = 0; j < resultArr.length; j++) {
 				$("#result").append(charArray[i].name + " can win by fighting in this order: "
@@ -36,23 +38,34 @@ function checkStats() {
 			$("#result").append("<b>" + charArray[i].name + " cannot win.</b><br/>");
 		}
 
+		// Adds an extra line for the next character
 		$("#result").append("<br/>");
 	}
 
+	// Recursive check for all winning battle orderings (permutations)
 	function checkAllEnemies(indexArr, player) {
 		debugger;
-		var currentIndexArr = indexArr.slice();;
+		var currentIndexArr = indexArr.slice();;  // Copies inderArr to new array
 
+		// For all possible characters
 		for (var i = 0; i < 4; i++) {
+
+			// If the character is not the player and has not already been fought
 			if ((i != player.index) && (currentIndexArr.indexOf(i) == -1)) {
-				var currentPlayer = doBattle(player, charArray[i]);
+				var currentPlayer = doBattle(player, charArray[i]);  // Fight!
+				
 				if (currentPlayer.won) {
-					currentIndexArr.push(i);
+					currentIndexArr.push(i);  // Add index of defeated enemy to currentIndexArr
+
+					// If three enemies have been defeated
 					if (currentIndexArr.length > 2) {
 						resultArr.push(currentIndexArr);
-					}
+					} // Else: recursive call to checkAllEnemies to fight next character
 					else {
 						checkAllEnemies(currentIndexArr, currentPlayer);
+						
+						// Execution returns here if previous check was unsuccessful
+						// Copies the indexArr of the previous recursive call over the failed recursive call's indexArr
 						currentIndexArr = indexArr.slice();
 					}
 				}
@@ -62,13 +75,15 @@ function checkStats() {
 
 	function doBattle(sourcePlayer, sourceEnemy) {
 		debugger;
-		var player = $.extend(true, {}, sourcePlayer);
+		var player = $.extend(true, {}, sourcePlayer);  // Copy player and enemy to temp objects
 		var enemy = $.extend(true, {}, sourceEnemy);
 
+		// Exits only when enemy or player is dead
 		while (true) {
 			enemy.hp -= player.currentAttack;
 			player.currentAttack += player.baseAttack;
 
+			// If enemy is dead, return true and prevent damage to player
 			if (enemy.hp <= 0) {
 				player.won = true;
 				return player;
